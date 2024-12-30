b4c2cd7ce569dc2a9e022c78ee38631e68df435a
livvaa <100385411+livvaa@users.noreply.github.com>
2024-12-30 13:57:01 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ BASE_URLS_FILE="base_urls.txt"                # Base URL'ler (ID|TakmaAd|URL)
 USER_BASES_FILE="user_bases.txt"              # Kullanıcıya ait base ID listesi (username|1,2,3)
 NGINX_CONFIG="/etc/nginx/sites-available/hls" # Nginx konfigürasyon dosyası
 PID_DIR="/var/run"                            # ffmpeg PID dosyalarının saklanacağı dizin
-SCRIPT_URL="https://raw.githubusercontent.com/livvaa/TS2HLS-Manager/main/ts2hls_live_management.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/livvaa/TS2HLS-Manager/main/test.sh"
 
 DOMAIN="fighttv.org"
 EMAIL="" # Let's Encrypt için e-posta adresi
